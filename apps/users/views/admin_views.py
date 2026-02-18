a435b7e033197e6e07ec2373e2414d5f0f924b63
diego-reb <dzgarcia10@gmail.com>
2026-02-17 18:26:04 -0600

    Validacion de rol

--- apps/users/views/admin_views.py
+++ apps/users/views/admin_views.py
@@ -7,8 +7,8 @@ from django.views.decorators.cache import never_cache
 
 @never_cache
 def panel_admin(request):
-    #if not request.session.get('usuario_id'):
-     #   return redirect('sesion')
+    if not request.session.get('usuario_id'):
+        return redirect('sesion')
 
     admins = Usuario.objects.select_related('rol', 'institucion') \
         .filter(rol__nombre_rol='Administrador')
